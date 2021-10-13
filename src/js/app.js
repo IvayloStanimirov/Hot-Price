654b78cc6896c9bd647fea94117cb129e0a06eb1
Ivaylo <kolevivaylo27@gmail.com>
2021-10-13 15:35:35 +0300

    Adding the javascript in app.js

--- src/js/app.js
+++ src/js/app.js
@@ -2,4 +2,10 @@ import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
+
+  let prices = document.querySelectorAll('.price');
+  let fireIcon = document.createTextNode('🔥')
+  let fireElement = document.createElement('span');
+  
+  prices[2].children[0].style.display = 'inline-block'
 });
